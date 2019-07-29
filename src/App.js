2fc0155e9fa5396b0afb4075a1bd93fa88327a5a
Jimmy MacGregor <jimmy.macgregor@futurenet.com>
2019-07-29 21:08:06 +0100

    Hello Sarah

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello Jimmy</p>
+        <p>Hello Sarah</p>
         <a
           className="App-link"
           href="https://reactjs.org"
